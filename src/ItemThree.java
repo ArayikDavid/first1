09cda9f1a3c751250465b852a8c408b34369b6e0
ovsep <ovsepyan.ara@gmail.com>
2019-04-11 21:15:32 +0300

    add itemtree

---
+++ src/ItemThree.java
@@ -0,0 +1,13 @@
+/**autor Best java developer))
+ * see System \n
+ * see System \t
+ *
+ */
+public class ItemThree {
+    public static void main(String[] args) {
+        System.out.println("H\ne\nl\nl\no");
+        System.out.println("W\to\tr\tl\td\t");
+
+    }
+
+}

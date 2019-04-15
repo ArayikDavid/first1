cb8470bfc895451c80b0f6237c913005cdf3ced3
ovsep <ovsepyan.ara@gmail.com>
2019-04-15 21:46:22 +0300

    add itemtree

--- src/ItemTwoo.java
+++ src/ItemTwoo.java
@@ -1,7 +1,7 @@
-import java.sql.SQLOutput;
+
 
 public class ItemTwoo {
     public static void main(String[] args) {
-        System.out.print("Овсепян Ара");
+                               System.out.print("Овсепян Ара");
     }
 }

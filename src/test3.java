9fc4491b7b4858f3b81c734292ec70955b2ace4e
ovsep <ovsepyan.ara@gmail.com>
2019-04-16 21:05:27 +0300

    add test3

---
+++ src/test3.java
@@ -0,0 +1,14 @@
+public class test3 {
+
+    public static void main(String[] args) {
+        byte g = 10;
+        short o = 100;
+        int i = 1000;
+        long l = 10000;
+        float f = 0.456f;
+        double d = 35348957.324235;
+        char c = 'a';
+        boolean is = d >= o;
+        System.out.println(g+" "+o+" "+i+" "+l+" "+f+" "+d+" "+c);
+    }
+}
